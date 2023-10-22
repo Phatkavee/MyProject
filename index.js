18250addf651c6399e63e8c21e6ddcf79529cb9d
phatkavee <s6506022630031@email.kmutnb.ac.th>
2023-10-22 19:21:39 +0700

    uu

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3');
 const app = express();
 
-const db = new sqlite3.Database('./db.sqlite');
+const db = new sqlite3.Database('./Database/db.sqlite');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
